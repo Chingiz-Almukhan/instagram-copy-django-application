b901cb5d7b7ff2546f914daba72b149b775c6b32
Almukhan Chingiz <chingizpain@gmail.com>
2022-11-01 13:08:54 +0600

    Откорректировал шаблоны просмотра страницы, входа и регистрации, редактирования обновил формы, прописал пути ко вьюхам в urls

--- instagaram/accounts/forms.py
+++ instagaram/accounts/forms.py
@@ -9,14 +9,25 @@ class LoginForm(forms.Form):
     next = forms.CharField(required=False, widget=forms.HiddenInput)
 
 
+GENDER = (('male', 'Мужской'), ('female', 'Женский'), ('other', 'Другое'))
+
+
 class CustomUserCreationForm(forms.ModelForm):
+    username = forms.CharField(min_length=5, max_length=50,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    email = forms.CharField(min_length=7, max_length=70, required=True,
+                            widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}))
+    avatar = forms.ImageField(required=True)
     password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
     password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
                                        widget=forms.PasswordInput)
+    gender = forms.ChoiceField(label='Пол', choices=GENDER, widget=forms.RadioSelect())
 
     class Meta:
         model = get_user_model()
-        fields = ('username', 'password', 'password_confirm', 'first_name', 'last_name', 'email', 'avatar', 'birthday')
+        fields = (
+            'username', 'email', 'avatar', 'password', 'password_confirm', 'first_name', 'biography', 'phone_number',
+            'gender')
 
     def clean(self):
         cleaned_data = super().clean()
@@ -28,14 +39,14 @@ class CustomUserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data.get('password'))
-
         if commit:
             user.save()
         return user
 
 
 class UserChangeForm(forms.ModelForm):
+    gender = forms.ChoiceField(label='Пол', choices=GENDER, widget=forms.RadioSelect)
+
     class Meta:
         model = get_user_model()
-        fields = ('first_name', 'last_name', 'email', 'avatar', 'birthday')
-        labels = {'first_name': 'Имя', 'last_name': 'Фамилия', 'email': 'Email'}
+        fields = ('username', 'first_name', 'last_name', 'biography', 'phone_number', 'email', 'avatar', 'gender')
--- instagaram/accounts/urls.py
+++ instagaram/accounts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView
+from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView, SubscribeAddView, \
+    SubscribersView, FollowView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
-    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change'),
+    path('subscribe/<int:pk>', SubscribeAddView.as_view(), name='subscribe'),
+    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='edit_profile'),
+    path('profile/<int:pk>/subscribers', SubscribersView.as_view(), name='subscribers'),
+    path('profile/<int:pk>/follows', FollowView.as_view(), name='follows'),
     path('logout/', logout_view, name='logout')
 ]
